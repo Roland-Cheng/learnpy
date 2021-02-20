f07a8f027a6fd2d1f700b117664da35b149d0c1a
Roland-Cheng <1347531203@qq.com>
2021-02-20 20:10:13 +0800

    time

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-s = input("input:")
-y = 1
-x = y+1
-print(121, s, x)
+import time
+
+if __name__ == "__main__":
+    s = input("input:")
+    y = 1
+    x = y+1
+    print(121, s, x)
+
+    time.sleep(3)
